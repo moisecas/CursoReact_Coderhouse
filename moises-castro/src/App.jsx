34b31d6fbbf11c3d04a3ecead0fce237933f54f8
Your Name <moisesjuniorcas20@gmail.com>
2022-07-04 10:02:42 -0500

    se modifica app.jsx

---
+++ moises-castro/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="./src/logo.svg" alt="moises" />
+      <input type="text" placeholder="Search" />
+      <input type="text" placeholder="Search" />
+      <hr />
+      <div>
+        Moiso
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
