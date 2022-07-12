fea6e3dcb264339f2cb6170bed72cd98bf49e590
Your Name <moisesjuniorcas20@gmail.com>
2022-07-12 15:25:10 -0500

    agregando itemlistcontainer

---
+++ ecommerce-moises/src/Components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = ({saludo}) => { //saludo es un parametro que se le pasa al componente ItemListContainer desde el componente App
+  return (
+    <div  >{saludo}</div> //saludo es un parametro que se le pasa al componente ItemListContainer desde el componente App
+  )
+}
+
+export default ItemListContainer
