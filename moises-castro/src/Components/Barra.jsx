96d93c036f6ca78078051c215e09c3aa074d7bf3
Your Name <moisesjuniorcas20@gmail.com>
2022-07-25 14:18:30 -0500

    name

--- moises-castro/src/Components/Barra.jsx
+++ moises-castro/src/Components/Barra.jsx
@@ -11,7 +11,8 @@ const Barra = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+         
+        <Navbar.Brand href="#home">ShopCoder</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
