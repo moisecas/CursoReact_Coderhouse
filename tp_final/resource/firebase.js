3a84a9cd664ed5f36028bcef5609b686cb88bcea
Your Name <moisesjuniorcas20@gmail.com>
2022-08-08 16:17:07 -0500

    preparando tp final de curso

---
+++ tp_final/resource/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBw6AR1BqGGWAMkntBDULmaCOAJ_Iux9PE",
+  authDomain: "shopcoder-8ab74.firebaseapp.com",
+  projectId: "shopcoder-8ab74",
+  storageBucket: "shopcoder-8ab74.appspot.com",
+  messagingSenderId: "45664403677",
+  appId: "1:45664403677:web:1b9fbad38d3e87b9821b34"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//npm install firebase
